2f6f697748e18f1c48e0988490b67c5157dd2497
naufalrays <rayhannaufalherlano@gmail.com>
2025-04-16 13:21:59 +0700

    Change vercel configuration

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get('/log', (req, res) => {
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3001, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
